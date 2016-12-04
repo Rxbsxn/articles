06259eb8c0ecadf428e82f85dbc254fec87bc59e
Robert Harężlak <harezlak.robert7@gmail.com>
2016-12-04 15:45:31 +0100

    pagition to fix

--- app/serializers/application.js
+++ app/serializers/application.js
@@ -11,6 +11,34 @@ export default DS.JSONAPISerializer.extend({
     }
 
     return result;
+  },
+
+  createPageMeta(data) {
+
+    let meta = {};
+
+    Object.keys(data).forEach(type => {
+      const link = data[type];
+      meta[type] = {};
+      let a = document.createElement('a');
+      a.href = link;
+
+      a.search.slice(1).split('&').forEach(pairs => {
+        const [param, value] = pairs.split('=');
+
+        if (param === 'page%5Bnumber%5D') {
+          meta[type].number = parseInt(value);
+        }
+        if (param === 'page%5Bsize%5D') {
+          meta[type].size = parseInt(value);
+        }
+
+      });
+      a = null;
+    });
+
+    return meta;
+
   }
 
 });
