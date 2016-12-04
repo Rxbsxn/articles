505384b24792843507fa498016626cb479da5412
Robert Harężlak <harezlak.robert7@gmail.com>
2016-12-04 15:30:12 +0100

    generated serializer for app data

---
+++ app/serializers/application.js
@@ -0,0 +1,16 @@
+import DS from 'ember-data';
+
+export default DS.JSONAPISerializer.extend({
+
+  normalizeQueryResponse(store, clazz, payload) {
+    const result = this._super(...arguments);
+    result.meta = result.meta || {};
+
+    if (payload.links) {
+      result.meta.pagination = this.createPageMeta(payload.links);
+    }
+
+    return result;
+  }
+
+});
